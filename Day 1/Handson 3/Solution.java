09e799563fe1aa7d77491154d94e8dd59942f593
Akshit Kumar <36480340+akshitchandora@users.noreply.github.com>
2020-05-12 08:07:33 +0530

    Final

---
+++ Day 1/Handson 3/Solution.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static int[] leftRotate(int []arr){
+        if(arr.length==1)
+        return arr;
+        int fitem=arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            arr[i-1]=arr[i];
+        }
+        arr[arr.length-1]=fitem;
+        return arr;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        for(int i=1;i<=d;i++)
+        a=leftRotate(a);
+
+        for(int e:a)
+        System.out.print(e+" ");
+        scanner.close();
+    }
+}
